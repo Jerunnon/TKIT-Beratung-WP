1de63afbe4bcaad5118ecc2126abfba0cd7d0215
Jerunnon <simon-info@gmx.net>
2020-06-07 16:26:48 +0200

    Footer and css

--- footer.php
+++ footer.php
@@ -1,10 +1,19 @@
 
 
+<section id="footer">
+  <div class="row">
+    <div class="col-lg-12">
+      <footer>
+        <p>&copy;André Klein@TKIT-Beratung - <?php echo date('Y'); ?></p>
+        <nav class="footer-nav">
+          <a href="">Kontakt</a>
+          <a href="">Impressum</a>
+        </nav>
+      </footer>
 
-    <footer>
-      <p>Copyright &copy; - <?php echo date('Y'); ?></p>
-    </footer>
-
-    <?php wp_footer(); ?>
+      <?php wp_footer(); ?>
+    </div>
+  </div>
+</section>
   </body>
 </html>
